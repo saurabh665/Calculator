0e04dd03cb52d54b9970472a9c8b2080130ea361
saurabh665 <saurabh.yadav.7760@gmail.com>
2024-02-07 18:10:54 +0530

    Search name

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [data, setData] = useState([]);
+  const [records, setRecords] = useState([]);
+useEffect(() => {
+    fetch('https://randomuser.me/api/?page=3&results=10')
+      .then((response) => response.json())
+      .then((result) => {
+        setData(result.results);
+        setRecords(result.results);
+      })
+      .catch((error) => console.log(error));
+  }, []); 
 
+
+const filter = (e) => {
+  const filteredData = data.filter(
+    (user) =>user.name.first.indexOf(e.target.value)>-1);
+  setRecords(filteredData);
+};
+
+  return (
+   <>
+    <input type="text"className="form-control"onChange={filter}placeholder="Enter Your name"/>
+    <div className="grid-user">
+    {records.map((user, index) => (
+      <ol>
+      <div className="pic">
+      <img src={user.picture.medium}/>
+        </div>
+          <>
+        {user.name.title} {user.name.first} {user.name.last}
+        <p>{user.login.username}
+          </p>
+          </>
+      
+       </ol>
+    ))
+    }
+  </div>
+</>
+
+);
+};
 export default App;
